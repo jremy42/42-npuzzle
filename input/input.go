bd462445a36f332696a7f1b63150c78037b1758f
jremy <jonathan.prudent13@gmail.com>
2023-06-27 15:58:18 +0200

    update scanword

--- input/input.go
+++ input/input.go
@@ -30,6 +30,8 @@ func isSpace(r rune) bool {
 
 func ScanWords(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	// Skip leading spaces.
+
+	fmt.Println("ScanWords")
 	start := 0
 	for width := 0; start < len(data); start += width {
 		var r rune
