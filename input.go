ff40c4cb31b60e951fecfdd730ba6b8ad0eba95d
jonathan <jonathan.prudent13@gmail.com>
2023-07-04 11:12:55 +0200

    refacto readinput

--- input.go
+++ input.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
-	"unicode/utf8"
+	"strings"
 )
 
 type Board struct {
@@ -23,73 +23,56 @@ func OpenFile(filename string) *os.File {
 	return file
 }
 
-func isSpace(r rune) bool {
-	return r == ' ' || r == '\t' || r == '\n' || r == '\r'
-}
-
-func scanWords(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	// Skip leading spaces.
-	start := 0
-	for width := 0; start < len(data); start += width {
-		var r rune
-		r, width = utf8.DecodeRune(data[start:])
-
-		// Skip comments.
-		if r == '#' {
-			for i := start; i < len(data); i++ {
-				if data[i] == '\n' {
-					start = i
-					break
-				}
-			}
-		}
-		if !isSpace(r) {
-			break
-		}
-	}
-	// Scan until space, marking end of word.
-	for width, i := 0, start; i < len(data); i += width {
-		var r rune
-		r, width = utf8.DecodeRune(data[i:])
-		if r == '#' {
-			return i, data[start:i], nil
-		}
-		if isSpace(r) {
-			return i + width, data[start:i], nil
-		}
-	}
-	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
-	if atEOF && len(data) > start {
-		return len(data), data[start:], nil
-	}
-	// Request more data.
-	return start, nil, nil
-}
-
 func ParseInput(file *os.File) (size int, board [][]int) {
 	scanner := bufio.NewScanner(file)
-	scanner.Split(scanWords)
+	scanner.Split(bufio.ScanLines)
+	inputArray := readInputArray(scanner)
+	size = extractSize(inputArray)
+	board = createBoard(size, inputArray)
+	file.Close()
+	return size, board
+}
+
+func readInputArray(scanner *bufio.Scanner) []int {
 	inputArray := make([]int, 0, 100)
 
 	for scanner.Scan() {
-		word := scanner.Text()
-		if word == "" {
+		line := scanner.Text()
+		comment := strings.Index(line, "#")
+		if comment != -1 {
+			line = line[:comment]
+		}
+		line = strings.TrimSpace(line)
+
+		if line == "" {
 			continue
 		}
-		num, err := strconv.Atoi(word)
-		if err != nil {
-			fmt.Println("Error parsing input")
-			os.Exit(1)
+
+		words := strings.Fields(line)
+		for _, word := range words {
+			num, err := strconv.Atoi(word)
+			if err != nil {
+				fmt.Println("Error parsing input")
+				os.Exit(1)
+			}
+			inputArray = append(inputArray, num)
 		}
-		inputArray = append(inputArray, num)
 	}
-	size = inputArray[0]
+
+	return inputArray
+}
+
+func extractSize(inputArray []int) int {
+	size := inputArray[0]
 	if size*size != len(inputArray)-1 {
 		fmt.Println("Error parsing input")
 		os.Exit(1)
 	}
+	return size
+}
 
-	board = make([][]int, size)
+func createBoard(size int, inputArray []int) [][]int {
+	board := make([][]int, size)
 	for i := 0; i < size; i++ {
 		board[i] = make([]int, size)
 		for j := 0; j < size; j++ {
@@ -100,6 +83,5 @@ func ParseInput(file *os.File) (size int, board [][]int) {
 			board[i][j] = inputArray[i*size+j+1]
 		}
 	}
-	file.Close()
-	return
+	return board
 }
