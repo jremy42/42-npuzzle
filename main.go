d60b9f7b64d6f2133fccb7953c56b94d01940b87
Frederic Le Blay <frederic.leblay@gmail.com>
2023-07-06 09:07:59 +0200

    setup recherche complet

--- main.go
+++ main.go
@@ -37,21 +37,30 @@ func getNextNodeIndex(queue []Node) int {
 	return ret
 }
 
-func getNextMoves(startPos, goalPos [][]int, scoreFx func(pos, startPos, goalPos [][]int, path []byte) int, path []byte, currentNode Node, seenNodes []Node) (nextPaths [][]byte, nextNodes []Node, nextSeen []Node) {
+func getNextMoves(startPos, goalPos [][]int, scoreFx func(pos, startPos, goalPos [][]int, path []byte) int, path []byte, currentNode Node, seenNodes []Node, elapsed *[4]time.Duration) (nextPaths [][]byte, nextNodes []Node, nextSeen []Node) {
 	for key, fx := range directions {
+		start := time.Now()
 		ok, nextPos := fx(currentNode.world)
+		end := time.Now()
+		elapsed[1] += end.Sub(start)
 		if !ok {
 			continue
 		}
 		score := scoreFx(nextPos, startPos, goalPos, path)
 		nextNode := Node{nextPos, score}
+		start = time.Now()
 		alreadyExplored := posAlreadySeen(seenNodes, nextPos)
+		end = time.Now()
+		elapsed[2] += end.Sub(start)
 		if alreadyExplored.world == nil ||
 			score < alreadyExplored.score {
+			start = time.Now()
 			toAdd := DeepSliceCopyAndAdd(path, key)
 			nextPaths = append(nextPaths, toAdd)
 			nextNodes = append(nextNodes, nextNode)
 			nextSeen = append(nextSeen, nextNode)
+			end = time.Now()
+			elapsed[3] += end.Sub(start)
 		}
 	}
 	return
@@ -64,7 +73,7 @@ func algo(world [][]int, scoreFx func(pos, startPos, goalPos [][]int, path []byt
 	posQueue := DeepSliceCopyAndAdd(seenNodes)
 	pathQueue := [][]byte{{}}
 
-	var elapsed time.Duration
+	var elapsed [4]time.Duration
 	for ; len(posQueue) > 0; tries++ {
 		maxSizeQueue = Max(maxSizeQueue, len(posQueue))
 
@@ -74,7 +83,7 @@ func algo(world [][]int, scoreFx func(pos, startPos, goalPos [][]int, path []byt
 		currentPath = pathQueue[nextIndex]
 
 		if tries > 0 && tries%1000 == 0 {
-			fmt.Printf("%d k tries. Len of try : %d. Score : %d\n",tries/1000, len(currentPath), currentNode.score)
+			fmt.Printf("%d k tries. Len of try : %d. Score : %d\n", tries/1000, len(currentPath), currentNode.score)
 		}
 
 		posQueue = append(posQueue[:nextIndex], posQueue[nextIndex+1:]...)
@@ -86,14 +95,17 @@ func algo(world [][]int, scoreFx func(pos, startPos, goalPos [][]int, path []byt
 		//fmt.Printf("current path %v\n", currentPath)
 		//time.Sleep(0 * time.Millisecond)
 		if isEqual(goalPos, currentNode.world) {
-			fmt.Println("getNexMoves total time :", elapsed.String())
+			fmt.Println("getNexMoves total time :", elapsed[0].String())
+			fmt.Println("time applying moves:", elapsed[1].String())
+			fmt.Println("time finding if node already exists in nodes list:", elapsed[2].String())
+			fmt.Println("time adding node to queues:", elapsed[3].String())
 			return
 		}
 
 		start := time.Now()
-		nextPaths, nextPoses, nextSeen := getNextMoves(startPos, goalPos, scoreFx, currentPath, currentNode, seenNodes)
+		nextPaths, nextPoses, nextSeen := getNextMoves(startPos, goalPos, scoreFx, currentPath, currentNode, seenNodes, &elapsed)
 		end := time.Now()
-		elapsed += end.Sub(start)
+		elapsed[0] += end.Sub(start)
 
 		//fmt.Println("next Paths", nextPaths)
 		posQueue = append(posQueue, nextPoses...)
